688fff469f30e72cfb00be040c7f36326e6c34e4
Sunny Suárez Saldaña <yo_soyalegre@hotmail.com>
2024-10-19 12:32:29 -0600

    Implementa la biblioteca armadillo para matrices

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+#include <vector>
+#include <armadillo>
 #include "include/Vertex.h"
 #include "include/Animation.h"
 
 using namespace std;
 
 int main(){
+    /*
     Animation an;
     Vertex P1(2, -3, 0);
     Vertex P2(-5, 6, 4);
@@ -15,4 +18,15 @@ int main(){
     Vertex Pc(9, 3, 4) ;
     Vertex Pd(8, 0, 6);
     vector<Vertex> curve = an.bezier(Pa, Pb, Pc, Pd, 0.1);
+    */
+arma::Mat<float> T = {{1, 1, 1, 1}};
+arma::Mat<float> Mb = {
+                    {-1, 3, -3, 1},
+                    {3, -6, 3, 0},
+                    {-3, 3, 0, 0},
+                    {1, 0, 0, 0}
+                    };
+arma::Mat<float> res = T * Mb;
+cout << res << endl;
+    return 0;
 }
