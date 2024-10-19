d91d4555de0f7d541032a58d17150a3030066f9f
Sunny Suárez Saldaña <yo_soyalegre@hotmail.com>
2024-10-18 20:35:14 -0600

    Implementa curvas de Bezier por formula

--- main.cpp
+++ main.cpp
@@ -30,10 +30,11 @@ int main(){
         */
 
        //Curvas de Hermite
-       Vertex P1(2, -3, 0);
-       Vertex P4(-5, 6, 4);
-       Vertex R1(5, 5, 1);
-       Vertex R4(-5, 3, -2);
+       /*
+       Vertex P1(0, 0, 0);
+       Vertex P4(10, 0, 0);
+       Vertex R1(5, 5, 0);
+       Vertex R4(-5, 5, 0);
        float dt = 0.1;
 
        for(float t=0.0; t<1.0+dt; t=t+dt){
@@ -44,5 +45,23 @@ int main(){
             cout << endl << "t = " << t << endl;
             P.print();
        }
+       */
+
+      //Curvas de Bezier
+    Vertex P1(0, 10, 0);
+    Vertex P2(3, 7, 2);
+    Vertex P3(9, 3, 4);
+    Vertex P4(8, 0, 6);
+    float dt = 0.1;
+
+    for(float t=0.0; t<1.0+dt; t=t+dt){
+        Vertex P = (P1 * pow((1 - t), 3)) +
+                    (P2 * (3*t*pow((1-t), 2))) + 
+                    (P3 * (3*pow(t, 2) * (1 - t))) + 
+                    (P4 * (pow(t,3)));
+        cout << endl << "t = " << t << endl;
+        P.print();
+    }
+
        
 }
