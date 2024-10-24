2d14ee1582e3d09eca543cb5e4ae58de4f1597c9
Sunny Suárez Saldaña <yo_soyalegre@hotmail.com>
2024-10-23 21:53:57 -0600

    Tarea del 7 de octubre

--- main.cpp
+++ main.cpp
@@ -32,6 +32,8 @@ int main(){
         }
     }
     */
+
+   /*
    Animation an;
    Vertex Pt(1, 0, 0);
    arma::Mat<float> Rz90 = an.rotation_z(90); //Matriz de rotacion de 90 grados en el eje z
@@ -42,7 +44,31 @@ int main(){
    Pt.print();
    cout << endl;
    cout << Pt90 << endl;
-    cout << Ptn90 << endl;
+   cout << Ptn90 << endl;
+    */
+
+   Animation an;
+   Vertex v1(3.0, 4.5, 1.2);
+   Vertex v2(6.4, 7.1, 2.2);
+
+   //Paso 1 - Trasladar los puntos al origen
+   arma::Mat<float> v1p = an.traslation(-v1.get_x(), -v1.get_y(), -v1.get_z()) * v1.homogeneous();
+   arma::Mat<float> v2p = an.traslation(-v1.get_x(), -v1.get_y(), -v1.get_z()) * v2.homogeneous();
+
+    //Paso 2 - Rotar el punto v2p al eje x
+    arma::Mat<float> v1pp = an.rotation_y(16.39) * v1p;
+    arma::Mat<float> v2pp = an.rotation_y(16.39) * v2p;
+
+    //Paso 3 - Rotar el punto v2pp al eje z
+    arma::Mat<float> v1ppp = an.rotation_z(-36.26) * v1pp;
+    arma::Mat<float> v2ppp = an.rotation_z(-36.26) * v2pp;
+
+    cout<<v1ppp<<endl;
+    cout<<v2ppp<<endl;
+    
+
+
+
 
     return 0;
 }
