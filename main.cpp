4f312c14fe67e7561da295a1626a4adc2d2fc6fd
Sunny Suárez Saldaña <yo_soyalegre@hotmail.com>
2024-12-01 13:07:06 -0600

    Quick test of cross and normalise methods of armadillo library

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "include/Cannon.h"
-#include "include/OpenGL.h"
+//#include "include/Cannon.h"
+//#include "include/OpenGL.h"
+#include "include/Vertex.h"
+#include <armadillo>
 
 using namespace std;
 
 int main(){
+    /*
     Cannon myCannon;
 
     OpenGL gl = OpenGL();
@@ -44,5 +47,18 @@ int main(){
 
     } while ( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
         glfwWindowShouldClose(window) == 0 );
+    */
+
+   Vertex v1(3.0, 4.0, 0.0);
+   Vertex v2(-1.0, 1.0, 1.0);
+   Vertex v3(6.0, 0.0, 2.0);
+
+   arma::vec va = {v2.get_x() - v1.get_x(), v2.get_y() - v1.get_y(), v2.get_z() - v1.get_z()};
+   arma::vec vb = {v3.get_x() - v1.get_x(), v3.get_y() - v1.get_y(), v3.get_z() - v1.get_z()};
+
+    arma::vec vc = arma::cross(va, vb);
+    arma::vec nvc = arma::normalise(vc);
+    cout << nvc;
+
     return 0;
 }
