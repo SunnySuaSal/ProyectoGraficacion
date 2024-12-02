03de345b76a4d22dfb81f05655c8e13801c82cab
Sunny Suárez Saldaña <yo_soyalegre@hotmail.com>
2024-12-01 19:39:38 -0600

    Replaces models and uncomments main

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 
 int main(){
-    /*
+    
     Cannon myCannon;
 
     OpenGL gl = OpenGL();
@@ -47,18 +47,6 @@ int main(){
 
     } while ( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
         glfwWindowShouldClose(window) == 0 );
-    */
-
-   Vertex v1(3.0, 4.0, 0.0);
-   Vertex v2(-1.0, 1.0, 1.0);
-   Vertex v3(6.0, 0.0, 2.0);
-
-   arma::vec va = {v2.get_x() - v1.get_x(), v2.get_y() - v1.get_y(), v2.get_z() - v1.get_z()};
-   arma::vec vb = {v3.get_x() - v1.get_x(), v3.get_y() - v1.get_y(), v3.get_z() - v1.get_z()};
-
-    arma::vec vc = arma::cross(va, vb);
-    arma::vec nvc = arma::normalise(vc);
-    cout << nvc;
 
     return 0;
 }
